ad11fa1bf60e2753eae0f5da274dcc62e6ab0101
LouisTenebrae <lme10112002@gmail.com>
2024-07-23 14:18:41 -0400

    testing for new lint

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,15 +1,17 @@
-export function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
-  }
+function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
+}
 
-  export function getLast() {
-    return ' is okay';
-  }
+function getLast() {
+  return ' is okay';
+}
 
-  export function taskNext() {
-    let combination = 'But sometimes let';
-    combination += getLast();
-  
-    return combination;
-  }
+function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
+
+module.exports = { taskFirst, getLast, taskNext };
