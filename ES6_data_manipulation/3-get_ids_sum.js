c45d23ec486150d5dd25d208ddcdb4b8ab86bc9e
LouisTenebrae <lme10112002@gmail.com>
2024-07-25 12:57:39 -0400

    lint

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,3 @@
 export default function getStudentIdsSum(studentList) {
-  return studentList.reduce(
-    (acummulator, student) => acummulator + student.id, 0,
-  );
+  return studentList.reduce((acummulator, student) => acummulator + student.id, 0);
 }
