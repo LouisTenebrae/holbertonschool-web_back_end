972dd3d184e97905b83f8efa8f38986f28eaf3b1
LouisTenebrae <lme10112002@gmail.com>
2024-07-25 20:31:47 -0400

    6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,20 +2,18 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUpPromise = signUpUser(firstName, lastName);
-  const uploadPromise = uploadPhoto(fileName);
-
-  return Promise.allSettled([signUpPromise, uploadPromise])
-    .then((results) => results.map((result) => {
-      if (result.status === 'fulfilled') {
-        return {
-          status: result.status,
-          value: result.value,
-        };
-      }
-      return {
-        status: result.status,
-        value: result.reason,
-      };
-    }));
+  const promise1 = signUpUser(firstName, lastName);
+  const promise2 = uploadPhoto(fileName);
+  return Promise.allSettled([promise1, promise2]).then((results) => results.map((result) => {
+    const newObject = {
+      status: result.status,
+    };
+    // assing value to resolved promise else reason of error
+    if (result.status === 'fulfilled') {
+      newObject.value = result.value;
+    } else {
+      newObject.value = result.reason.toString();
+    }
+    return newObject;
+  }));
 }
