21a975a768e5192b82232cdd5a26444d0b1dc466
LouisTenebrae <lme10112002@gmail.com>
2024-08-20 16:05:20 -0400

    4(?

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -9,3 +9,4 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     '''Creates a task that waits for n random amounts of time'''
     return await wait_n(n, max_delay)
+ame 
